6f9e5889b3746c19921dec1081b53120edf153e1
TemidayoII <olabidayzii@gmail.com>
2026-04-29 11:51:31 -0400

    Implement search API with filters

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -2,41 +2,39 @@ const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
 
-// Search for gifts
 router.get('/', async (req, res, next) => {
-    try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
+	try {
+		const db = await connectToDatabase();
+		const collection = db.collection("gifts");
 
-        const collection = db.collection("gifts");
+		let query = {};
 
-        // Initialize the query object
-        let query = {};
+		// name filter
+		if (req.query.name && req.query.name.trim() !== '') {
+			query.name = { $regex: req.query.name, $options: "i" };
+		}
 
-        // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
-            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
+		// category filter
+		if (req.query.category) {
+			query.category = req.query.category;
+		}
 
-        // Task 3: Add other filters to the query
-        if (req.query.category) {
-            // {{insert code here}}
-        }
-        if (req.query.condition) {
-            // {{insert code here}} 
-        }
-        if (req.query.age_years) {
-            // {{insert code here}}
-            query.age_years = { $lte: parseInt(req.query.age_years) };
-        }
+		// condition filter
+		if (req.query.condition) {
+			query.condition = req.query.condition;
+		}
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
+		// age filter
+		if (req.query.age_years) {
+			query.age_years = { $lte: parseInt(req.query.age_years) };
+		}
 
-        res.json(gifts);
-    } catch (e) {
-        next(e);
-    }
+		const gifts = await collection.find(query).toArray();
+		res.json(gifts);
+
+	} catch (e) {
+		next(e);
+	}
 });
 
 module.exports = router;
